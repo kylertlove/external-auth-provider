00af41f8fc0707098e2d4945af8e752eeefea974
Kyler Love <kyler.love@gmail.com>
2025-09-01 16:38:13 -0400

    init

---
+++ src/main/java/org/kyler/external_auth/ExternalAuthApplication.java
@@ -0,0 +1,13 @@
+package org.kyler.external_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kyler/external_auth/ExternalAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kyler.external_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
